dd9fc06fbc1b886e9d1c431a73c14d58faddc8bb
alpinweb <alpinweb@yandex.ru>
2023-04-24 10:59:02 +0600

    format AuthFormWidget

--- src/view/widgets/AuthForm.widget.ts
+++ src/view/widgets/AuthForm.widget.ts
@@ -1,14 +1,14 @@
-import {AbstractView} from "../../framework/interface/AbstractView";
-import {TextInputComponent} from "../ui-components/TextInput.component";
-import {ButtonComponent} from "../ui-components/Button.component";
+import { AbstractView } from "../../framework/interface/AbstractView";
+import { TextInputComponent } from "../ui-components/TextInput.component";
+import { ButtonComponent } from "../ui-components/Button.component";
 
 interface Props {
-    user: userData;
+  user: userData;
 }
 
 interface State {
-    title: string;
-    user: Props["user"];
+  title: string;
+  user: Props["user"];
 }
 
 const createAuthFormTemplate = (state: State, components: Components) => `
@@ -21,50 +21,50 @@ const createAuthFormTemplate = (state: State, components: Components) => `
 </Form>
 `;
 
-export class AuthFormWidget extends AbstractView{
-    protected state: State = {
-        title: 'Введите даннеы аккаунта',
-        user: {
-            login: '',
-            password: '',
-            email: '',
-        },
-    };
-    private components: Components = {
-        LoginTextInput: null,
-        PasswordTextInput: null,
-        EmailTextInput: null,
-        FormButton: null,
-    };
-    constructor(props: Props) {
-        super();
-        this.state.user = props.user;
-        this.components.LoginTextInput = new TextInputComponent({
-            value: this.state.user.login,
-            placeholder: 'Введите логин',
-            isDisabled: false,
-            isError: false,
-            isPassword: false,
-        });
-        this.components.PasswordTextInput = new TextInputComponent({
-            value: this.state.user.password,
-            placeholder: 'Введите пароль',
-            isDisabled: false,
-            isError: false,
-            isPassword: true,
-        });
-        this.components.EmailTextInput = new TextInputComponent({
-            value: this.state.user.email,
-            placeholder: 'Введите email',
-            isDisabled: false,
-            isError: false,
-            isPassword: true,
-        });
-        this.components.FormButton = new ButtonComponent({
-            title: 'Отправить',
-        });
-    }
-    get template(): string {
-        return createAuthFormTemplate(this.state, this.components);
-    }
+export class AuthFormWidget extends AbstractView {
+  protected state: State = {
+    title: "Введите даннеы аккаунта",
+    user: {
+      login: "",
+      password: "",
+      email: "",
+    },
+  };
+  private components: Components = {
+    LoginTextInput: null,
+    PasswordTextInput: null,
+    EmailTextInput: null,
+    FormButton: null,
+  };
+  constructor(props: Props) {
+    super();
+    this.state.user = props.user;
+    this.components.LoginTextInput = new TextInputComponent({
+      value: this.state.user.login,
+      placeholder: "Введите логин",
+      isDisabled: false,
+      isError: false,
+      isPassword: false,
+    });
+    this.components.PasswordTextInput = new TextInputComponent({
+      value: this.state.user.password,
+      placeholder: "Введите пароль",
+      isDisabled: false,
+      isError: false,
+      isPassword: true,
+    });
+    this.components.EmailTextInput = new TextInputComponent({
+      value: this.state.user.email,
+      placeholder: "Введите email",
+      isDisabled: false,
+      isError: false,
+      isPassword: true,
+    });
+    this.components.FormButton = new ButtonComponent({
+      title: "Отправить",
+    });
+  }
+  get template(): string {
+    return createAuthFormTemplate(this.state, this.components);
+  }
 }
