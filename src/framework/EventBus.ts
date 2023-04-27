e5939e8d4171da9c49157db97e741ada86a88f7a
alpinweb <alpinweb@yandex.ru>
2023-04-27 11:06:05 +0600

    create EventBus

---
+++ src/framework/EventBus.ts
@@ -0,0 +1,24 @@
+interface Listeners {
+  [key: string]: [] | ((...args: any) => void)[];
+}
+
+export class EventBus {
+  private listeners: Listeners = {};
+
+  on(event: string, callback: (...args: any) => void) {
+    this.listeners[event] = this.listeners[event] || [];
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.listeners[event].push(callback);
+    return () => {
+      this.listeners[event] = this.listeners[event].filter((listener) => {
+        return listener !== callback;
+      });
+    };
+  }
+  emit(event: string, ...args: never[]) {
+      this.listeners[event].forEach((listener) => {
+          listener(...args);
+      });
+  }
+}
