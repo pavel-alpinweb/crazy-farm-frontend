6ecb2fb1cc4ae2b3aed1a84650ecbead0c0e84c4
alpinweb <alpinweb@yandex.ru>
2023-05-03 11:19:51 +0600

    listen url change & init controller

--- src/framework/Router.ts
+++ src/framework/Router.ts
@@ -1,4 +1,18 @@
+import AuthController from "../controler/auth.controller";
+
+declare global {
+    type controller = AuthController;
+    interface RouterParams {
+        url: string,
+        controller: controller,
+    }
+}
+
 export class Router {
+    private params: Array<RouterParams> = [];
+    constructor(params: Array<RouterParams>) {
+        this.params = params;
+    }
     static get path(): string {
         const url = new URL(window.location.href);
         return  url.hash.slice(1);
@@ -8,10 +22,18 @@ export class Router {
         window.location.replace(path);
     }
 
+    private changePageHandler = () => {
+        const route: any = this.params.find((item: any) => item.url === Router.path);
+        if (route) {
+            route.controller.methods.init();
+        } else {
+            Router.push('/#/404');
+        }
+    }
+
     public init(): void {
+        window.addEventListener('hashchange', this.changePageHandler);
         Router.push('/#/');
-        window.addEventListener('hashchange', () => {
-            console.log('url changed', Router.path);
-        });
+        this.changePageHandler();
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,18 @@ import { EventBus } from "./framework/EventBus";
 
 export const eventBus: EventBus = new EventBus();
 const userModel: User = new User();
-const router: Router = new Router();
 const authController = new AuthController(userModel);
+
+const params: Array<RouterParams> = [
+    {
+        url: '/',
+        controller: authController,
+    },
+    {
+        url: '/login',
+        controller: authController,
+    }
+];
+
+const router: Router = new Router(params);
 router.init();
-authController.methods.init();
