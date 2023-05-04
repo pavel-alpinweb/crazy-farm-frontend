24de7cc50f45aa7f3100ce2835f69c9b499410ab
alpinweb <alpinweb@yandex.ru>
2023-05-04 10:02:42 +0600

    create Farm screen

---
+++ src/view/screens/Farm.screen.ts
@@ -0,0 +1,13 @@
+import {AbstractStaticScreen} from "../../framework/interface/AbstractStaticScreen";
+
+const createFarmScreenTemplate = () => `
+<div class="farm-screen">
+    <h1>Farm screen</h1>
+</div>
+`;
+export class FarmScreen extends AbstractStaticScreen {
+    get template(): string {
+        return createFarmScreenTemplate();
+    }
+    
+}
