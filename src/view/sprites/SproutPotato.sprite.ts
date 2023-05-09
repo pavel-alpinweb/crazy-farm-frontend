53a1746f2c315732bba5b10a1a8a549cbbef332a
alpinweb <alpinweb@yandex.ru>
2023-05-09 19:08:14 +0600

    step

--- src/view/sprites/SproutPotato.sprite.ts
+++ src/view/sprites/SproutPotato.sprite.ts
@@ -3,7 +3,7 @@ import { ANIMATED_SPRITE_URL } from "../../utils/constants";
 
 export class SproutPotatoSprite extends AbstractAnimatedSprite {
   protected animationSpeed = 0.07;
-  protected spriteFrames: Array<string> = [
+  protected spriteFrames = [
     `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-1.frame.png`,
     `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-2.frame.png`,
     `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-3.frame.png`,
