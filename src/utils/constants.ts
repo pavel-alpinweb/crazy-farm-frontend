eac1b7b8187cb7c16352b0e566c00e5682ca7b66
alpinweb <alpinweb@yandex.ru>
2023-05-09 18:04:10 +0600

    create ANIMATED_SPRITE_URL constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const appContainer: Element | null = document.getElementById("app");
 export const STATIC_SPRITE_URL = './assets/img/sprites/static';
+export const ANIMATED_SPRITE_URL = './assets/img/sprites/animated';
