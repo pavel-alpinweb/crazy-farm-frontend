19df13427687c097c7c9be2eea7b3372b3d9cba8
alpinweb <alpinweb@yandex.ru>
2023-05-09 18:19:34 +0600

    create test sprite

--- src/view/scenes/Farm.scene.ts
+++ src/view/scenes/Farm.scene.ts
@@ -1,5 +1,6 @@
+import * as PIXI from "pixi.js";
 import {AbstractScene} from "../../framework/graphics/AbstractScene";
-import {STATIC_SPRITE_URL} from "../../utils/constants";
+import {STATIC_SPRITE_URL, ANIMATED_SPRITE_URL} from "../../utils/constants";
 import {GroundSprite} from "../sprites/Ground.sprite";
 
 export class FarmScene extends AbstractScene{
@@ -16,9 +17,24 @@ export class FarmScene extends AbstractScene{
     }
 
     protected renderSprites(): void {
-        if (this.sprites.ground?.sprite) {
-            this.container?.addChild(this.sprites.ground.sprite);
+        // if (this.sprites.ground?.sprite) {
+        //     this.container?.addChild(this.sprites.ground.sprite);
+        // }
+        const spriteFrames = [
+            `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-1.frame.png`,
+            `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-2.frame.png`,
+            `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-3.frame.png`,
+        ];
+        const textureArray = [];
+        for (const frame of spriteFrames) {
+            const texture = PIXI.Texture.from(frame);
+            textureArray.push(texture);
         }
+        const animatedSprite = new PIXI.AnimatedSprite(textureArray);
+        animatedSprite.anchor.set(0.5);
+        animatedSprite.animationSpeed = 0.07;
+        animatedSprite.play();
+        this.container?.addChild(animatedSprite);
     }
 
 }
