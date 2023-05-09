ab9d96388ce656bb0f7e7fa65ee964ad8030d8de
alpinweb <alpinweb@yandex.ru>
2023-05-09 11:58:50 +0600

    create class AbstractStaticSprite

---
+++ src/framework/graphics/AbstractStaticSprite.ts
@@ -0,0 +1,24 @@
+import * as PIXI from "pixi.js";
+
+export class AbstractStaticSprite {
+    private readonly texture: PIXI.Texture | null = null;
+    private renderedSprite: PIXI.Sprite | null = null;
+    constructor(assetUrl: string) {
+        this.texture = PIXI.Texture.from(assetUrl);
+    }
+
+    private render(): PIXI.Sprite | null {
+        let sprite = null;
+        if (this.texture) {
+            sprite = new PIXI.Sprite(this.texture);
+        }
+        return sprite;
+    }
+
+    public get sprite(): PIXI.Sprite | null {
+        if (!this.renderedSprite) {
+            this.renderedSprite = this.render();
+        }
+        return this.renderedSprite;
+    }
+}
