18ece9b79b67cf50da7fbf97fae7fd17b9bb33ed
alpinweb <alpinweb@yandex.ru>
2023-05-09 11:39:45 +0600

    create container in AbstractScene

--- src/framework/graphics/AbstractScene.ts
+++ src/framework/graphics/AbstractScene.ts
@@ -3,8 +3,9 @@ import * as PIXI from "pixi.js";
 export class AbstractScene {
   private renderedElement: Element | null = null;
   private scene: PIXI.Application | null = null;
-    protected events: Events = {};
-    public emits: Emits = {};
+  public container: PIXI.Container | null = null;
+  protected events: Events = {};
+  public emits: Emits = {};
 
   private render(): Element | null {
     const newElement = document.createElement("div");
@@ -13,6 +14,10 @@ export class AbstractScene {
       background: "#1099bb",
       view: canvas,
     });
+    this.container = new PIXI.Container();
+    this.scene.stage.addChild(this.container);
+    this.container.x = this.scene.screen.width / 2;
+    this.container.y = this.scene.screen.height / 2;
     newElement.append(canvas);
     return newElement;
   }
