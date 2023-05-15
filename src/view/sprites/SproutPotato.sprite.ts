2437b7437fbbe1c85814558e3771a65343df33ba
alpinweb <alpinweb@yandex.ru>
2023-05-15 09:01:04 +0600

    format code

--- src/view/sprites/SproutPotato.sprite.ts
+++ src/view/sprites/SproutPotato.sprite.ts
@@ -2,10 +2,10 @@ import { AbstractAnimatedSprite } from "../../framework/graphics/AbstractAnimate
 import { ANIMATED_SPRITE_URL } from "../../utils/constants";
 
 export class SproutPotatoSprite extends AbstractAnimatedSprite {
-  protected animationSpeed = 0.07;
   protected spriteFrames = [
     `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-1.frame.png`,
     `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-2.frame.png`,
     `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato-3.frame.png`,
   ];
+  protected animationSpeed = 0.07;
 }
