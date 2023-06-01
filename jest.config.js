c5241eadc53dfd0f71fa22d707f81a4f1512c24b
alpinweb <alpinweb@yandex.ru>
2023-06-01 13:08:55 +0600

    step

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  extensionsToTreatAsEsm: ['.ts'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   "moduleDirectories": ["node_modules", "src"],
