80d3c3ef2a9630ffb29b739ef5a5f5558b55f648
alpinweb <alpinweb@yandex.ru>
2023-06-05 15:00:12 +0600

    create mock registrationFirstStep

--- src/mock/auth.mock.ts
+++ src/mock/auth.mock.ts
@@ -18,3 +18,31 @@ export function updateUserDataMock(data: UserData): Promise<UserData> {
     }, 2000);
   });
 }
+
+export function registrationFirstStep(
+  data: UserData,
+  isSuccess = true
+): Promise<string | HttpError> {
+  if (isSuccess) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Confirmation email sent to email");
+      }, 2000);
+    });
+  } else {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          timestamp: "yyyy-MM-dd hh:mm:ss.SSS",
+          httpStatus: "http status short description",
+          httpErrorCode: 400,
+          reasons: [
+            {
+              message: "string",
+            },
+          ],
+        });
+      }, 2000);
+    });
+  }
+}
