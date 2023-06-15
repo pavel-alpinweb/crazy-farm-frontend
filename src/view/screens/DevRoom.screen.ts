bc4523512e6d6185a771fa8c44cb1a30cea0f7c8
alpinweb <alpinweb@yandex.ru>
2023-06-15 11:56:48 +0600

    create class DevRoomScreen

---
+++ src/view/screens/DevRoom.screen.ts
@@ -0,0 +1,54 @@
+import {AbstractScreen} from "../../framework/interface/AbstractScreen";
+import {DEFAULT_FARM_STATE} from "../../utils/constants";
+import {FarmScene} from "../scenes/Farm.scene";
+
+interface State {
+    farm: FarmState;
+}
+
+const createDevRoomScreenTemplate = () => `
+<div class="dev-room">
+    <div class="dev-room__scene" data-slot-scene></div>
+</div>
+`;
+
+export class DevRoomScreen extends AbstractScreen{
+    protected controllerMethods: Methods = {};
+    protected components: ScreenComponents = {
+        MainScene: null,
+    };
+    protected state: State = {
+        farm: DEFAULT_FARM_STATE,
+    };
+
+    constructor() {
+        super();
+        this.initComponents();
+        this.renderComponents();
+    }
+
+    protected initComponents(): void {
+        this.components.MainScene = new FarmScene({ farm: this.state.farm });
+    }
+
+    protected renderComponents(): void {
+        this.mountComponent('scene', this.components.MainScene);
+    }
+
+    protected setEvents(): void {
+        console.warn('Init: DevRoom Events');
+    }
+
+    protected setState(): void {
+        console.warn('Init: DevRoom state');
+    }
+
+    get template(): string {
+        return createDevRoomScreenTemplate(this.state);
+    }
+
+    public remove(): void {
+        this.components.MainScene?.remove();
+        super.remove();
+    }
+}
