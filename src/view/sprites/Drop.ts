a93fc03eb1a4171a223c37fd702c26b9dbcd997a
alpinweb <alpinweb@yandex.ru>
2023-06-19 10:02:32 +0600

    create needs sprites

---
+++ src/view/sprites/Drop.ts
@@ -0,0 +1,6 @@
+import {AbstractStaticSprite} from "../../framework/graphics/AbstractStaticSprite";
+import {STATIC_SPRITE_URL} from "../../utils/constants";
+
+export class DropSprite extends AbstractStaticSprite{
+    protected spriteURL = `${STATIC_SPRITE_URL}/drop.sprite.png`;
+}
