3fa8d25986d1f1dd353ba82f16dbebd8dd77ed9f
alpinweb <alpinweb@yandex.ru>
2023-06-20 11:02:34 +0600

    create tool component

---
+++ src/view/ui-components/Tool.component.ts
@@ -0,0 +1,39 @@
+import {AbstractView} from "../../framework/interface/AbstractView";
+
+interface Props {
+    name: string;
+}
+
+interface State {
+    name: Props["name"];
+}
+
+const createToolTemplate = (state: State) => `
+<div class="tool tool--${state.name}"></div>
+`;
+
+export class ToolComponent extends AbstractView{
+    protected state: State = {
+        name: '',
+    };
+    constructor(props: Props) {
+        super();
+        this.setState(props);
+        this.setEvents();
+    }
+
+    protected setEvents(): void {
+        this.emits.setClickEvent = (callback: (data: Concrete) => void) => {
+            this.events.click = callback;
+        };
+    }
+
+    protected setState(props: Props): void {
+        this.state.name = props.name;
+    }
+
+    get template(): string {
+        return createToolTemplate(this.state);
+    }
+    
+}
