4325a8a03e081318902f90ae6ae597d0d8250259
alpinweb <alpinweb@yandex.ru>
2023-06-20 11:42:50 +0600

    create active state

--- src/view/ui-components/Tool.component.ts
+++ src/view/ui-components/Tool.component.ts
@@ -1,39 +1,44 @@
-import {AbstractView} from "../../framework/interface/AbstractView";
+import { AbstractView } from "../../framework/interface/AbstractView";
 
 interface Props {
-    name: string;
+  name: string;
 }
 
 interface State {
-    name: Props["name"];
+  name: Props["name"];
+  isActive: boolean;
 }
 
 const createToolTemplate = (state: State) => `
-<div class="tool tool--${state.name}"></div>
+    <div 
+        class="tool 
+        tool--${state.name} 
+        ${state.isActive ? "tool--active" : ""}"
+    ></div>
 `;
 
-export class ToolComponent extends AbstractView{
-    protected state: State = {
-        name: '',
+export class ToolComponent extends AbstractView {
+  protected state: State = {
+    name: "",
+    isActive: false,
+  };
+  constructor(props: Props) {
+    super();
+    this.setState(props);
+    this.setEvents();
+  }
+
+  protected setEvents(): void {
+    this.emits.setClickEvent = (callback: (data: Concrete) => void) => {
+      this.events.click = callback;
     };
-    constructor(props: Props) {
-        super();
-        this.setState(props);
-        this.setEvents();
-    }
+  }
 
-    protected setEvents(): void {
-        this.emits.setClickEvent = (callback: (data: Concrete) => void) => {
-            this.events.click = callback;
-        };
-    }
+  protected setState(props: Props): void {
+    this.state.name = props.name;
+  }
 
-    protected setState(props: Props): void {
-        this.state.name = props.name;
-    }
-
-    get template(): string {
-        return createToolTemplate(this.state);
-    }
-    
+  get template(): string {
+    return createToolTemplate(this.state);
+  }
 }
