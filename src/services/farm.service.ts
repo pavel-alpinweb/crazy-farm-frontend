e80a1e4831d3826ab6de4afa046aba04f01d5dab
alpinweb <alpinweb@yandex.ru>
2023-06-28 10:40:54 +0600

    create service getJwtForConnection

---
+++ src/services/farm.service.ts
@@ -0,0 +1,20 @@
+import {AxiosResponse} from "axios";
+import Service from "../framework/Service";
+
+declare global {
+    interface Jws {
+        jws: string;
+    }
+}
+
+export default class FarmService {
+    static async getJwtForConnection(token: string): Promise<Jws> {
+        const result: AxiosResponse<Jws> = await Service.get("/game/getJwtForConnection", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        return result.data;
+    }
+}
