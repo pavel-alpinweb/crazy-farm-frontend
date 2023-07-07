c763d607b0d71de44db1106b46c8c355903f0382
alpinweb <alpinweb@yandex.ru>
2023-07-07 11:29:59 +0600

    refactoring animated sprites

--- src/view/sprites/KidPotato.sprite.ts
+++ src/view/sprites/KidPotato.sprite.ts
@@ -1,12 +1,7 @@
 import { AbstractAnimatedSprite } from "../../framework/graphics/AbstractAnimatedSprite";
-import { ANIMATED_SPRITE_URL } from "../../utils/constants";
 
 export class KidPotatoSprite extends AbstractAnimatedSprite {
-  protected spriteFrames = [
-    `${ANIMATED_SPRITE_URL}/kid-potato/kid-potato-1.frame.png`,
-    `${ANIMATED_SPRITE_URL}/kid-potato/kid-potato-2.frame.png`,
-    `${ANIMATED_SPRITE_URL}/kid-potato/kid-potato-3.frame.png`,
-    `${ANIMATED_SPRITE_URL}/kid-potato/kid-potato-4.frame.png`,
-  ];
-  protected animationSpeed = 0.04;
+  protected spriteName = 'kid-potato';
+  protected framesNumber = 4;
+  protected animationSpeed = 0.05;
 }
