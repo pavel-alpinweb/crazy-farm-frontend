0d9dc856b28d63ceeec88b2ebae3ad896190a6c3
alpinweb <alpinweb@yandex.ru>
2023-07-13 10:44:38 +0600

    add methods for rendering

--- src/compositions/RenderScene.composition.ts
+++ src/compositions/RenderScene.composition.ts
@@ -1,3 +1,71 @@
+import * as PIXI from "pixi.js";
+
 export class RenderSceneComposition {
-    
+    public renderContainer(container: Container, scene: PIXI.Application): void {
+        if (container.render) return;
+        else {
+            container.render = new PIXI.Container();
+            scene.stage.addChild(container.render);
+        }
+    }
+
+    public setContainerX(container: Container, x: number): void {
+        if (container.render) {
+            container.render.x = x;
+        }
+    }
+
+    public setContainerY(container: Container, y: number): void {
+        if (container.render) {
+            container.render.y = y;
+        }
+    }
+
+    public setContainerPivotX(container: Container, x: number): void {
+        if (container.render) {
+            container.render.pivot.x = x;
+        }
+    }
+
+    public setContainerPivotY(container: Container, y: number): void {
+        if (container.render) {
+            container.render.pivot.y = y;
+        }
+    }
+
+    public centerContainer(container: Container, scene: PIXI.Application): void {
+        if (scene) {
+            this.setContainerX(container, scene.screen.width / 2);
+            this.setContainerY(container, scene.screen.height / 2);
+        }
+    }
+
+    public centerPivotContainer(container: Container): void {
+        if (container.render) {
+            this.setContainerPivotX(container, container.render.width / 2);
+            this.setContainerPivotY(container, container.render.height / 2);
+        }
+    }
+
+    public addSprite(
+        container: Container,
+        sprite: PIXI.Sprite | PIXI.AnimatedSprite | null | undefined
+    ): void {
+        if (container.render && sprite) {
+            container.render.addChild(sprite);
+        }
+    }
+
+    public removeSprite(
+        container: Container,
+        sprite: PIXI.Sprite | PIXI.AnimatedSprite | null | undefined
+    ): void {
+        if (container.render && sprite) {
+            container.render.removeChild(sprite);
+        }
+    }
+
+    public removeAllSprites(container: Container) {
+        container.render?.removeChildren();
+    }
 }
