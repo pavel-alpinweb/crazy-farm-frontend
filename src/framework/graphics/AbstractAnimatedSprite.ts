c0d549e488335549acd0e52311cda4a5f4692664
alpinweb <alpinweb@yandex.ru>
2023-07-17 11:28:17 +0600

    create checking for sprite format

--- src/framework/graphics/AbstractAnimatedSprite.ts
+++ src/framework/graphics/AbstractAnimatedSprite.ts
@@ -6,19 +6,26 @@ export abstract class AbstractAnimatedSprite {
   private renderedSprite: PIXI.AnimatedSprite | null = null;
   protected abstract animationSpeed: number;
   protected abstract spriteName: string;
+  protected isDataSprite = false;
   protected abstract framesNumber: number;
 
   private render(): PIXI.AnimatedSprite | null {
-    for (let i = 1; i <= this.framesNumber; i++) {
-      const texture = PIXI.Texture.from(
-        `${ANIMATED_SPRITE_URL}/${this.spriteName}/${this.spriteName}-${i}.frame.png`
-      );
-      this.textureArray.push(texture);
+    let animatedSprite = null;
+    if (!this.isDataSprite) {
+      for (let i = 1; i <= this.framesNumber; i++) {
+        const texture = PIXI.Texture.from(
+            `${ANIMATED_SPRITE_URL}/${this.spriteName}/${this.spriteName}-${i}.frame.png`
+        );
+        this.textureArray.push(texture);
+      }
+      animatedSprite = new PIXI.AnimatedSprite(this.textureArray);
     }
-    const animatedSprite = new PIXI.AnimatedSprite(this.textureArray);
-    animatedSprite.anchor.set(0.5);
-    animatedSprite.animationSpeed = this.animationSpeed;
-    animatedSprite.play();
+
+    if (animatedSprite) {
+      animatedSprite.anchor.set(0.5);
+      animatedSprite.animationSpeed = this.animationSpeed;
+      animatedSprite?.play();
+    }
 
     return animatedSprite;
   }
