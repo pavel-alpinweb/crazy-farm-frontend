8aba56986ec86bcd9aa309eb391b90deb0f329d0
alpinweb <alpinweb@yandex.ru>
2023-07-18 11:49:00 +0600

    change render sprite composition

--- src/framework/graphics/AbstractAnimatedSprite.ts
+++ src/framework/graphics/AbstractAnimatedSprite.ts
@@ -9,21 +9,6 @@ export abstract class AbstractAnimatedSprite {
   protected abstract framesNumber: number;
 
   private async render(): Promise<PIXI.AnimatedSprite | null> {
-    // let animatedSprite = null;
-    // PIXI.Assets.load(`${ANIMATED_SPRITE_URL}/${this.spriteName}/${this.spriteName}.json`).then(() => {
-    //   const animationsData = PIXI.Assets.cache.get(`${ANIMATED_SPRITE_URL}/${this.spriteName}/${this.spriteName}.json`).data;
-    //   const spritesheet = new PIXI.Spritesheet(
-    //       PIXI.BaseTexture.from(`${ANIMATED_SPRITE_URL}/${this.spriteName}/${this.spriteName}.png`),
-    //       animationsData
-    //   );
-    //   spritesheet.parse().then(() => {
-    //     animatedSprite = new PIXI.AnimatedSprite(spritesheet.animations[this.spriteName]);
-    //     animatedSprite.anchor.set(0.5);
-    //     animatedSprite.animationSpeed = this.animationSpeed;
-    //     animatedSprite.play();
-    //     return animatedSprite;
-    //   });
-    // });
     const animationsData = await PIXI.Assets.load(
       `${ANIMATED_SPRITE_URL}/${this.spriteName}/${this.spriteName}.json`
     );
