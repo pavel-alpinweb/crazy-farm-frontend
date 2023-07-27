f545b5e601868ca69f4433e82e5adc2ea283b853
alpinweb <alpinweb@yandex.ru>
2023-07-27 10:12:21 +0600

    create sprite manifest file

--- src/framework/graphics/AbstractScene.ts
+++ src/framework/graphics/AbstractScene.ts
@@ -1,8 +1,7 @@
 import * as PIXI from "pixi.js";
 import { AbstractStaticSprite } from "./AbstractStaticSprite";
 import { AbstractAnimatedSprite } from "./AbstractAnimatedSprite";
-import {ResolverManifest} from "pixi.js";
-import {ANIMATED_SPRITE_URL, STATIC_SPRITE_URL} from "../../utils/constants";
+import {manifest} from "../../assets/manifests/farm.manifest";
 
 declare global {
   type SingleSprite = AbstractStaticSprite | AbstractAnimatedSprite | null;
@@ -42,81 +41,6 @@ export abstract class AbstractScene {
       this.scene.renderer.view.style.touchAction = "auto";
     }
     this.scene.renderer.plugins.interaction.autoPreventDefault = false;
-    const manifest: ResolverManifest = {
-      bundles: [
-        {
-          name: 'dialog',
-          assets: {
-            sprite: `${STATIC_SPRITE_URL}/dialog.sprite.png`,
-          },
-        },
-        {
-          name: 'drop',
-          assets: {
-            sprite: `${STATIC_SPRITE_URL}/drop.sprite.png`,
-          },
-        },
-        {
-          name: 'bug',
-          assets: {
-            sprite: `${STATIC_SPRITE_URL}/bug.sprite.png`,
-          },
-        },
-        {
-          name: 'hunger',
-          assets: {
-            sprite: `${STATIC_SPRITE_URL}/hunger.sprite.png`,
-          },
-        },
-        {
-          name: 'empty-field',
-          assets: {
-            sprite: `${STATIC_SPRITE_URL}/empty-field.sprite.png`,
-          },
-        },
-        {
-          name: 'land',
-          assets: {
-            sprite: `${STATIC_SPRITE_URL}/land.sprite.png`,
-          },
-        },
-        {
-          name: 'sprout-potato',
-          assets: {
-              sprite_sheet: `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato.png`,
-              sprite_data: `${ANIMATED_SPRITE_URL}/sprout-potato/sprout-potato.json`,
-          },
-        },
-        {
-          name: 'child-potato',
-          assets: {
-              sprite_sheet: `${ANIMATED_SPRITE_URL}/child-potato/child-potato.png`,
-              sprite_data: `${ANIMATED_SPRITE_URL}/child-potato/child-potato.json`,
-          },
-        },
-        {
-          name: 'kid-potato',
-          assets: {
-              sprite_sheet: `${ANIMATED_SPRITE_URL}/kid-potato/kid-potato.png`,
-              sprite_data: `${ANIMATED_SPRITE_URL}/kid-potato/kid-potato.json`,
-          },
-        },
-        {
-          name: 'adult-potato',
-          assets: {
-              sprite_sheet: `${ANIMATED_SPRITE_URL}/adult-potato/adult-potato.png`,
-              sprite_data: `${ANIMATED_SPRITE_URL}/adult-potato/adult-potato.json`,
-          },
-        },
-        {
-          name: 'ghost-potato',
-          assets: {
-              sprite_sheet: `${ANIMATED_SPRITE_URL}/ghost-potato/ghost-potato.png`,
-              sprite_data: `${ANIMATED_SPRITE_URL}/ghost-potato/ghost-potato.json`,
-          },
-        },
-      ]
-    };
     PIXI.Assets.init({ manifest }).then(() => {
       PIXI.Assets.loadBundle(manifest.bundles.map(bundle => bundle.name)).then((bundles) => {
         console.log('Load bundle', bundles);
