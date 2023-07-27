51e95869b5b6cdb3a4094e1dfd48bdf2c5bbf52d
alpinweb <alpinweb@yandex.ru>
2023-07-27 10:23:17 +0600

    realise class AssetsLoader

--- src/framework/graphics/AssetsLoader.ts
+++ src/framework/graphics/AssetsLoader.ts
@@ -1,3 +1,16 @@
+import * as PIXI from "pixi.js";
+import {manifest} from "../../assets/manifests/farm.manifest";
+
 export class AssetsLoader {
-    
+    private readonly manifest!: string;
+    public bundle: PIXI.ResolverBundle | null = null;
+
+    constructor(manifestURl: string) {
+        this.manifest = manifestURl;
+    }
+
+    async load(): Promise<void> {
+        await PIXI.Assets.init({ manifest: this.manifest });
+        this.bundle = await  PIXI.Assets.loadBundle(manifest.bundles.map(bundle => bundle.name));
+    }
 }
