0a1b7695329b26040b8f7cda309d5f202cf1f54e
alpinweb <alpinweb@yandex.ru>
2023-07-27 11:20:19 +0600

    reset default state

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -29,7 +29,7 @@ export const DEFAULT_FARM_STATE: FarmState = {
       isBlocked: false,
       name: "central",
       character: {
-        type: 'potato',
+        type: 'empty',
         stage: 0,
         needs: [],
       },
