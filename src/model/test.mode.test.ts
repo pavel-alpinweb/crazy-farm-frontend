03712ff127bd95c3d0c07d53e691508befc06b62
alpinweb <alpinweb@yandex.ru>
2023-07-28 15:59:53 +0600

    remove example tests

--- src/model/test.mode.test.ts
+++
@@ -1,9 +0,0 @@
-import {TestModel} from "./test.model";
-
-describe("User Model getters:", () => {
-    test("doesn't throw error when constructed", () => {
-        expect(() => {
-            const testMyClass = new TestModel();
-        }).not.toThrowError();
-    });
-});
