ef7d00c981e9887af1e984570efaad67eafd88e4
alpinweb <alpinweb@yandex.ru>
2023-08-17 10:26:33 +0600

    create function template with two params

--- src/framework/Toaster.ts
+++ src/framework/Toaster.ts
@@ -1,3 +1,9 @@
+const template = (message: string, isSuccess: boolean): string => `
+    <div class="toaster ${isSuccess ? 'toaster--success' : 'toaster--error'}">
+        <span class="toaster__message">${message}</span>
+    </div>
+`;
+
 export class Toaster {
-    
+
 }
