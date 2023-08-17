41627926995c24cf9d3c9f7377b5be23c5a51641
alpinweb <alpinweb@yandex.ru>
2023-08-17 10:46:16 +0600

    create private methods add & public

--- src/framework/Toaster.ts
+++ src/framework/Toaster.ts
@@ -5,5 +5,19 @@ const template = (message: string, isSuccess: boolean): string => `
 `;
 
 export class Toaster {
+    private $toasterContainer!: null | HTMLElement;
 
+    private add($toaster: HTMLElement) {
+        if (!this.$toasterContainer) {
+            this.$toasterContainer = document.createElement('div');
+            this.$toasterContainer.classList.add('toaster-container');
+            const $body = document.querySelector('body');
+            $body?.appendChild(this.$toasterContainer);
+        }
+        this.$toasterContainer.appendChild($toaster);
+    }
+
+    private remove($toaster: HTMLElement) {
+        $toaster.remove();
+    }
 }
