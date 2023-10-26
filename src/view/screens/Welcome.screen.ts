cf30dad1fb6f10101829f14b7f791b0b4f4fcf41
alpinweb <alpinweb@yandex.ru>
2023-10-26 15:58:34 +0600

    create welcome-screen__character

--- src/view/screens/Welcome.screen.ts
+++ src/view/screens/Welcome.screen.ts
@@ -7,7 +7,12 @@ const createWelcomeScreenTamplate = () => `
             <a class="button brown" href="/#/login">Войти через почту</a>
         </div>
         <div class="welcome-screen__middle">
-            <img src="/assets/img/illustrations/potato.png" alt="potato">
+            <div class="welcome-screen__character">
+                <img src="/assets/img/illustrations/potato.png" alt="potato">
+                <div class="welcome-screen__text">
+                    Фермер, стой! Выбери портал, через который ты войдешь!
+                </div>
+            </div>
             <img src="/assets/img/illustrations/sprout.png" alt="potato">
         </div>
         <div class="welcome-screen__bottom">
