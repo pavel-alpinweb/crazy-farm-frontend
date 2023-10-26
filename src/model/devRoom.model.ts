f892632ef7d22bb26d67ff6c783bbb0d5dd9d295
alpinweb <alpinweb@yandex.ru>
2023-10-26 17:14:31 +0600

    create model for dev room

---
+++ src/model/devRoom.model.ts
@@ -0,0 +1,103 @@
+import { AbstractStaticSprite } from "../framework/graphics/AbstractStaticSprite";
+import { AbstractAnimatedSprite } from "../framework/graphics/AbstractAnimatedSprite";
+import { EventBus } from "../framework/EventBus";
+
+export const TOOLS: Tools = {
+    SHOVEL: "shovel",
+    BAILER: "bailer",
+    FERTILIZER: "fertilizer",
+    SPRAYER: "sprayer",
+    SEEDS: "seeds",
+    EMPTY: "empty",
+};
+
+export const TEST_FARM_STATE: FarmState = {
+    containers: [
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "0-0",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "1-0",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "2-0",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "3-0",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "0-1",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "1-1",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "2-1",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "3-1",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "0-2",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "1-2",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "2-2",
+            character: null,
+        },
+        {
+            isEmpty: true,
+            isBlocked: false,
+            name: "3-2",
+            character: null,
+        },
+    ],
+};
+
+export default class FarmModel {
+    private initialState: FarmData = {
+        farm: TEST_FARM_STATE,
+        activeTool: TOOLS.EMPTY,
+        player: {
+            cash: 0,
+        },
+    };
+
+    public get state(): FarmState {
+        return this.initialState.farm;
+    }
+}
--- src/view/screens/DevRoom.screen.ts
+++ src/view/screens/DevRoom.screen.ts
@@ -1,5 +1,4 @@
 import { AbstractScreen } from "../../framework/interface/AbstractScreen";
-import { DEFAULT_FARM_STATE, TOOLS } from "../../model/farm.model";
 import { DevScene } from "../scenes/Dev.scene";
 import { ToolComponent } from "../ui-components/Tool.component";
 import { ToolsSetWidget } from "../widgets/ToolsSet.widget";
@@ -7,6 +6,7 @@ import { TOOLS_PRICES } from "../../model/farm.model";
 import { WalletComponent } from "../ui-components/Wallet.component";
 import {TextInputComponent} from "../ui-components/TextInput.component";
 import {PageHeaderComponent} from "../ui-components/PageHeader.component";
+import {TEST_FARM_STATE, TOOLS} from "../../model/devRoom.model";
 
 interface State {
   farm: FarmState;
@@ -56,7 +56,7 @@ export class DevRoomScreen extends AbstractScreen {
     PageHeader: null,
   };
   protected state: State = {
-    farm: DEFAULT_FARM_STATE,
+    farm: TEST_FARM_STATE,
     player: {
       cash: 1000,
     },
