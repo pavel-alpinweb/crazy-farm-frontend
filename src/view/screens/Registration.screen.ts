09464fdd682767c92fc588a770145fb959534448
alpinweb <alpinweb@yandex.ru>
2023-11-03 15:12:11 +0600

    add translations for LoginScreen & RegistrationScreen

--- src/view/screens/Registration.screen.ts
+++ src/view/screens/Registration.screen.ts
@@ -1,6 +1,7 @@
 import { AbstractScreen } from "../../framework/interface/AbstractScreen";
 import { PageHeaderComponent } from "../ui-components/PageHeader.component";
 import { AuthFormWidget } from "../widgets/AuthForm.widget";
+import {LinkButtonComponent} from "../ui-components/LinkButton.component";
 
 interface Props {
   user: RegistrationData;
@@ -15,9 +16,7 @@ const createAuthScreenTemplate = () => `
 <div class="auth-screen">
     <div class="auth-screen__header" data-slot-header></div>
     <div class="auth-screen__content" data-slot-content></div>
-    <div class="auth-screen__footer">
-        <a class="button brown left" href="/#/registration-ways">Назад</a>
-    </div>
+    <div class="auth-screen__footer" data-slot-footer></div>
 </div>
 `;
 
@@ -35,6 +34,7 @@ export class RegistrationScreen extends AbstractScreen {
     PageHeaderComponent: null,
     UserInfoComponent: null,
     AuthFormWidget: null,
+    BackLinkComponent: null,
   };
   constructor(props: Props, methods: Methods) {
     super();
@@ -54,11 +54,17 @@ export class RegistrationScreen extends AbstractScreen {
     this.components.AuthFormWidget = new AuthFormWidget({
       user: this.state.user,
     });
+    this.components.BackLinkComponent = new LinkButtonComponent({
+      link: '/#/welcome',
+      translationKey: 'back',
+      classes: 'brown left',
+    });
   }
 
   protected renderComponents(): void {
     this.mountComponent("header", this.components.PageHeaderComponent);
     this.mountComponent("content", this.components.AuthFormWidget);
+    this.mountComponent("footer", this.components.BackLinkComponent);
   }
 
   protected setEvents(): void {
