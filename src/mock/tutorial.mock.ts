202fe6912124e0027a8e2808728ed987daaf2743
alpinweb <alpinweb@yandex.ru>
2023-11-10 12:00:26 +0600

    create tutorial mock function

---
+++ src/mock/tutorial.mock.ts
@@ -0,0 +1,29 @@
+import {DEFAULT_FARM_STATE} from "../model/farm.model";
+
+const tutorialFarmStateCells: Array<Cell> = JSON.parse(JSON.stringify(DEFAULT_FARM_STATE.containers)).map((cell: Cell) => {
+    if (cell.name !== "0-1") {
+        cell.isBlocked = true;
+    }
+});
+const playerCash = 1000;
+
+export function updateTutorial(
+    cell: string,
+    tool: tool
+): Promise<FarmResponse> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                player: {
+                    cash: playerCash,
+                },
+                tutorial: {
+                    isActive: false,
+                    currentStep: 0,
+                    blockedTools: [],
+                },
+                containers: tutorialFarmStateCells,
+            });
+        }, 2000);
+    });
+}
