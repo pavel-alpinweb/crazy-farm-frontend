fec9e65429ace623f6572941b59f46c07a1a5b44
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-04 15:02:37 +0600

    create test for method showRestartMessage

--- src/model/almanac.test.ts
+++ src/model/almanac.test.ts
@@ -1,90 +1,100 @@
-import {AlmanacModel} from "./almanac.model";
-import {beforeEach} from "@jest/globals";
-import {tutorialState} from "../mock/tutorial.mock";
+import { AlmanacModel } from "./almanac.model";
+import { beforeEach } from "@jest/globals";
+import { tutorialState } from "../mock/tutorial.mock";
 
 describe("Almanac Model:", () => {
-    let almanac: AlmanacModel;
+  let almanac: AlmanacModel;
 
-    beforeEach(() => {
-        almanac = new AlmanacModel();
+  beforeEach(() => {
+    almanac = new AlmanacModel();
+  });
+
+  test("doesn't throw error when Almanac Model constructed", () => {
+    expect(() => {
+      new AlmanacModel();
+    }).not.toThrowError();
+  });
+
+  test("method: setTutorialState", () => {
+    almanac.setTutorialState({
+      isActive: true,
+      currentStep: 1,
+      blockedTools: ["shovel", "bailer"],
     });
 
-    test("doesn't throw error when Almanac Model constructed", () => {
-        expect(() => {
-            new AlmanacModel();
-        }).not.toThrowError();
+    expect(almanac.tutorial).toEqual({
+      isActive: true,
+      currentStep: 1,
+      blockedTools: ["shovel", "bailer"],
     });
 
-    test("method: setTutorialState", () => {
-        almanac.setTutorialState({
-            isActive: true,
-            currentStep: 1,
-            blockedTools: ["shovel", "bailer"],
-        });
+    expect(almanac.state).toEqual({
+      isShow: true,
+      isActive: false,
+      currentTextKey: "tutorial.1",
+      currentActions: ["close"],
+    });
+  });
 
-        expect(almanac.tutorial).toEqual({
-            isActive: true,
-            currentStep: 1,
-            blockedTools: ["shovel", "bailer"],
-        });
-
-        expect(almanac.state).toEqual({
-            isShow: true,
-            isActive: false,
-            currentTextKey: 'tutorial.1',
-            currentActions: ["close"],
-        });
+  test("method: toggleAlmanac", () => {
+    // set tutorial
+    almanac.setTutorialState({
+      isActive: tutorialState[1].isActive,
+      currentStep: 1,
+      blockedTools: tutorialState[1].blockedTools,
     });
 
-    test("method: toggleAlmanac", () => {
-        // set tutorial
-        almanac.setTutorialState({
-            isActive: tutorialState[1].isActive,
-            currentStep: 1,
-            blockedTools: tutorialState[1].blockedTools,
-        });
+    // close almanac modal
+    almanac.toggleAlmanac();
 
-        // close almanac modal
-        almanac.toggleAlmanac();
+    // open almanac modal
+    almanac.toggleAlmanac();
 
-        // open almanac modal
-        almanac.toggleAlmanac();
-
-        expect(almanac.state).toEqual({
-            isActive: false,
-            isShow: true,
-            currentTextKey: 'tutorial.1',
-            currentActions: ["close"],
-        });
-
-        // set tutorial
-        almanac.setTutorialState({
-            isActive: false,
-            currentStep: 7,
-            blockedTools: [],
-        });
-
-        // close almanac modal
-        almanac.toggleAlmanac();
-
-        // open almanac modal
-        almanac.toggleAlmanac();
-
-        expect(almanac.state).toEqual({
-            isActive: false,
-            isShow: true,
-            currentTextKey: 'almanacDefault',
-            currentActions: ["show", "close"],
-        });
+    expect(almanac.state).toEqual({
+      isActive: false,
+      isShow: true,
+      currentTextKey: "tutorial.1",
+      currentActions: ["close"],
     });
 
-    test("method: showExitMessage", () => {
-        almanac.showExitMessage();
-        expect(almanac.state).toEqual({
-            isActive: false,
-            isShow: true,
-            currentTextKey: 'exit',
-            currentActions: ["exit", "no-close"],
-        });
+    // set tutorial
+    almanac.setTutorialState({
+      isActive: false,
+      currentStep: 7,
+      blockedTools: [],
     });
+
+    // close almanac modal
+    almanac.toggleAlmanac();
+
+    // open almanac modal
+    almanac.toggleAlmanac();
+
+    expect(almanac.state).toEqual({
+      isActive: false,
+      isShow: true,
+      currentTextKey: "almanacDefault",
+      currentActions: ["show", "close"],
+    });
+  });
+
+  test("method: showExitMessage", () => {
+    almanac.showExitMessage();
+    expect(almanac.state).toEqual({
+      isActive: false,
+      isShow: true,
+      currentTextKey: "exit",
+      currentActions: ["exit", "no-close"],
+    });
+  });
+
+  test("method: showRestartMessage", () => {
+    almanac.showRestartMessage();
+    expect(almanac.state).toEqual({
+      isActive: false,
+      isShow: true,
+      currentTextKey: "restart",
+      currentActions: ["restart", "no-close"],
+    });
+  });
 });
