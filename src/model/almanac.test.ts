3573ffe72430e058883a20ef465becd0c00f372d
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-04 14:57:18 +0600

    create test for method toggleAlmanac

--- src/model/almanac.test.ts
+++ src/model/almanac.test.ts
@@ -1,6 +1,14 @@
 import {AlmanacModel} from "./almanac.model";
+import {beforeEach} from "@jest/globals";
+import {tutorialState} from "../mock/tutorial.mock";
 
 describe("Almanac Model:", () => {
+    let almanac: AlmanacModel;
+
+    beforeEach(() => {
+        almanac = new AlmanacModel();
+    });
+
     test("doesn't throw error when Almanac Model constructed", () => {
         expect(() => {
             new AlmanacModel();
@@ -8,8 +16,6 @@ describe("Almanac Model:", () => {
     });
 
     test("method: setTutorialState", () => {
-        const almanac = new AlmanacModel();
-
         almanac.setTutorialState({
             isActive: true,
             currentStep: 1,
@@ -27,6 +33,48 @@ describe("Almanac Model:", () => {
             isActive: false,
             currentTextKey: 'tutorial.1',
             currentActions: ["close"],
+        });
+    });
+
+    test("method: toggleAlmanac", () => {
+        // set tutorial
+        almanac.setTutorialState({
+            isActive: tutorialState[1].isActive,
+            currentStep: 1,
+            blockedTools: tutorialState[1].blockedTools,
+        });
+
+        // close almanac modal
+        almanac.toggleAlmanac();
+
+        // open almanac modal
+        almanac.toggleAlmanac();
+
+        expect(almanac.state).toEqual({
+            isActive: false,
+            isShow: true,
+            currentTextKey: 'tutorial.1',
+            currentActions: ["close"],
+        });
+
+        // set tutorial
+        almanac.setTutorialState({
+            isActive: false,
+            currentStep: 7,
+            blockedTools: [],
+        });
+
+        // close almanac modal
+        almanac.toggleAlmanac();
+
+        // open almanac modal
+        almanac.toggleAlmanac();
+
+        expect(almanac.state).toEqual({
+            isActive: false,
+            isShow: true,
+            currentTextKey: 'almanacDefault',
+            currentActions: ["show", "close"],
         });
     });
 });
