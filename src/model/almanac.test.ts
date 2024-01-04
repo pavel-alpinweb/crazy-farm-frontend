c97a3ce14134f4aef28410923749e8643eff1464
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-04 14:41:47 +0600

    create test for method setTutorialState

--- src/model/almanac.test.ts
+++ src/model/almanac.test.ts
@@ -6,4 +6,27 @@ describe("Almanac Model:", () => {
             new AlmanacModel();
         }).not.toThrowError();
     });
+
+    test("method: setTutorialState", () => {
+        const almanac = new AlmanacModel();
+
+        almanac.setTutorialState({
+            isActive: true,
+            currentStep: 1,
+            blockedTools: ["shovel", "bailer"],
+        });
+
+        expect(almanac.tutorial).toEqual({
+            isActive: true,
+            currentStep: 1,
+            blockedTools: ["shovel", "bailer"],
+        });
+
+        expect(almanac.state).toEqual({
+            isShow: true,
+            isActive: false,
+            currentTextKey: 'tutorial.1',
+            currentActions: ["close"],
+        });
+    });
 });
